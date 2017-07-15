8d2fd1293815a341082ddbf5484e06105384a290
sunya9 <mugen.xyz@gmail.com>
2017-07-15 22:47:11 +0900

    Refactoring

--- src/commands.rs
+++ src/commands.rs
@@ -3,10 +3,12 @@ extern crate time;
 extern crate crypto;
 extern crate rusqlite;
 extern crate params;
+extern crate rand;
 
 use std::iter;
 use std::env;
 use std::process::{Command};
+use self::rand::Rng;
 use self::iron::prelude::*;
 use self::iron::status;
 use self::rusqlite::Connection;
@@ -15,26 +17,26 @@ use self::crypto::digest::Digest;
 use self::params::{Params, Value};
 use webhook::WebHook;
 
-pub fn new(cmd_arg: Option<String>) {
+pub fn new(iter: iter::Skip<env::Args>) {
   let con = get_connection();
   let mut sha256 = Sha256::new();
-  let input = time::now().rfc3339().to_string();
-  let salt = format!("{}{}", input, env::var("SALT").unwrap_or("DEBUG".to_string()));
+  let salt = rand::thread_rng().gen_ascii_chars().take(40).collect::<String>();
   sha256.input_str(&salt);
   let hash = sha256.result_str();
-  let command_str = match cmd_arg {
-    Some(val) => val,
-    None => panic!("Error! You have to set command to second parameter.")
-  };
+  let command = iter.map(|arg| arg.to_string()).collect::<Vec<String>>().join(" ");
+  if command.len() > 0 {
+    let mut stmt = con.prepare("INSERT INTO hooks (hash, command) VALUES (?1, ?2)").unwrap();
+    let id = stmt.insert(&[&hash, &command]).unwrap();
+    let hook = WebHook {
+      id: id,
+      hash: hash,
+      command: command
+    };
+    println!("{}", hook);
 
-  let mut stmt = con.prepare("INSERT INTO hooks (hash, command) VALUES (?1, ?2)").unwrap();
-  let id = stmt.insert(&[&hash, &command_str]).unwrap();
-  let hook = WebHook {
-    id: id,
-    hash: hash,
-    command: command_str
-  };
-  println!("{}", hook);
+  } else {
+    panic!("Error! You have to set command to second parameter.");
+  }
 }
 
 pub fn show() {
@@ -63,26 +65,34 @@ pub fn delete(iter: iter::Skip<env::Args>) {
 }
 
 pub fn start() {
-  Iron::new(handler).http("localhost:5123").unwrap();
+  let host = format!("localhost:{}", env::var("PORT").unwrap_or("5123".to_string()));
+  println!("Listening on http://{}", host);
+  Iron::new(handler).http(&host).unwrap();
 }
 
 fn handler(req: &mut Request) -> IronResult<Response> {
   let con = get_connection();
   let map = req.get_ref::<Params>().unwrap();
   if let Some(id) = map.get("id").and_then(convert_id) {
-    // let con = conn.lock().unwrap();
     let mut stmt = con.prepare("SELECT command FROM hooks where hash = ?1").unwrap();
     let row = stmt.query_row(&[&id], |row| row.get::<i32, String>(0));
     if let Ok(command) = row {
       let mut iter = command.split_whitespace();
       let output = Command::new(iter.next().unwrap())
         .args(iter)
-        .output()
-        .expect("failed");
-      if output.status.success() {
-        return Ok(Response::with((status::Ok, output.stdout)))
-      } else {
-        return Ok(Response::with((status::InternalServerError, "Command not found.")))
+        .output();
+      match output {
+        Ok(output_str) => {
+          let (status_code, out) = if output_str.status.success() {
+            (status::Ok, output_str.stdout)
+          } else {
+            (status::InternalServerError, output_str.stderr)
+          };
+          return Ok(Response::with((status_code, out)))
+        },
+        Err(err) => {
+          return Ok(Response::with((status::InternalServerError, err.to_string())))
+        }
       }
     }
   }
